270f4817d9d5dfac589798a0c523b9fd8237c00f
kywertheim <kywertheim@protonmail.com>
2022-02-02 20:35:53 +0000

    compilation failure

--- src/agents/schwann.cu
+++ src/agents/schwann.cu
@@ -106,12 +106,13 @@ __device__ __forceinline__ void Schwann_sense(flamegpu::DeviceAPI<flamegpu::Mess
             s_apop_signal += 1 * step_size;
             stress = 1;
         }
-	const float chemo0 = FLAMEGPU.environment.getProperty<float, 6>("chemo_effects", 0);
-        const float chemo1 = FLAMEGPU.environment.getProperty<float, 6>("chemo_effects", 1);
-        const float chemo2 = FLAMEGPU.environment.getProperty<float, 6>("chemo_effects", 2);
-        const float chemo3 = FLAMEGPU.environment.getProperty<float, 6>("chemo_effects", 3);
-        const float chemo4 = FLAMEGPU.environment.getProperty<float, 6>("chemo_effects", 4);
-        const float chemo5 = FLAMEGPU.environment.getProperty<float, 6>("chemo_effects", 5);
+	const int CHEMO_ACTIVE = FLAMEGPU->environment.getProperty<int>("CHEMO_ACTIVE");
+	const float chemo0 = FLAMEGPU->environment.getProperty<float, 6>("chemo_effects", 0);
+        const float chemo1 = FLAMEGPU->environment.getProperty<float, 6>("chemo_effects", 1);
+        const float chemo2 = FLAMEGPU->environment.getProperty<float, 6>("chemo_effects", 2);
+        const float chemo3 = FLAMEGPU->environment.getProperty<float, 6>("chemo_effects", 3);
+        const float chemo4 = FLAMEGPU->environment.getProperty<float, 6>("chemo_effects", 4);
+        const float chemo5 = FLAMEGPU->environment.getProperty<float, 6>("chemo_effects", 5);
         const glm::uvec4 cycle_stages = FLAMEGPU->environment.getProperty<glm::uvec4>("cycle_stages");
 	int chemo = 0;
 	if(CHEMO_ACTIVE && FLAMEGPU->random.uniform<float>()<(chemo0+chemo1+chemo2+chemo3+chemo4+chemo5)/6)
