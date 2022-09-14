97ac0ea94bd0952735d483ace0315a78e0f89250
Marek Kasztelnik <mkasztelnik@gmail.com>
2022-09-14 13:58:14 +0200

    Build and test model on Athena HPC

---
+++ scripts/test_run_on_athena.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+## Job name
+#SBATCH -J test_run_fgpu2nb
+## Number of allocated nodes
+#SBATCH -N 1
+## Number of tasks per node (by default this corresponds to the number of cores allocated per node)
+#SBATCH --ntasks-per-node=1
+## Memory allocated per core (default is 5GB)
+#SBATCH --mem-per-cpu=5GB
+## Max task execution time (format is HH:MM:SS)
+#SBATCH --time=00:10:00
+## Name of grant to which resource usage will be charged (primage1 or primage1gpu)
+#SBATCH -A plgprimage4-gpu-a100
+## Name of partition (plgrid-testing or plgrid-gpu)
+#SBATCH -p plgrid-gpu-a100
+## Specify that we want GPUs (if using plgrid-gpu)
+#SBATCH --gres=gpu:1
+## Name of file to which standard output will be redirected
+#SBATCH --output="test_run_output.out"
+## Name of file to which the standard error stream will be redirected
+#SBATCH --error="test_run_error.err"
+
+## Default pre-job stuff
+## srun /bin/hostname
+cd $SLURM_SUBMIT_DIR
+
+## Start job
+
+module load CUDA/11.7.0
+
+nvidia-smi
+
+./build/bin/Release/orchestrator_FGPUNB --in ../inputs/in-2022-03-29.json --primage ../inputs/out-2022-03-29.json
